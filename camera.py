1f1ccfda99fc80d9906827500c197b5be727201f
subice21 <40129011+subice21@users.noreply.github.com>
2020-05-28 04:15:09 +0300

    modifted the camera for find body
    and off website/flask -> showCam.py

--- camera.py
+++ camera.py
@@ -4,17 +4,23 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
 #where is the haarcascades?
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='D:/OpenCV/opencv/build/etc/haarcascades/haarcascade_frontalface_alt.xml')
-parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='D:/OpenCV/opencv/build/etc/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--face_cascade', help='Path to face cascade.', default='C:/openCV/opencv/build/etc/haarcascades/haarcascade_frontalface_alt.xml')
+parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='C:/openCV/opencv/build/etc/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--body_cascade', help='Path to face cascade.', default='C:/openCV/opencv/build/etc/haarcascades/haarcascade_upperbody.xml')
+
 #camera usbID
 parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
 args = parser.parse_args()
 face_cascade_name = args.face_cascade
 eyes_cascade_name = args.eyes_cascade
+body_cascade_name = args.body_cascade
 face_cascade = cv.CascadeClassifier()
 eyes_cascade = cv.CascadeClassifier()
+body_cascade = cv.CascadeClassifier()
 
-
+if not body_cascade.load(cv.samples.findFile(body_cascade_name)):
+    print('--(!)Error loading face cascade')
+    exit(0)
 if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
     print('--(!)Error loading face cascade')
     exit(0)
@@ -22,13 +28,16 @@ if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
     print('--(!)Error loading eyes cascade')
     exit(0)
 
-camera_device = args.camera    
+camera_device = args.camera 
 
 class VideoCamera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
+        self.bodyColor = [177, 177 , 177]
+        self.faceColor = [144, 144, 144]
+        self.eyeColor = [144, 255, 144]
         self.video = cv.VideoCapture(camera_device)
         if not self.video.isOpened:
             print('--(!)Error opening video capture')
@@ -50,18 +59,23 @@ class VideoCamera(object):
         frame_gray = cv.equalizeHist(frame_gray)
         #-- Detect faces
         faces = face_cascade.detectMultiScale(frame_gray)
-        faceColor = [144, 144, 144]
-        eyeColor = [255, 0, 255]
+        bodys = body_cascade.detectMultiScale(frame_gray)
+        #faceColor = [144, 144, 144]
+        #eyeColor = [144, 255, 144]
+        for (x1,y1,w1,h1) in bodys :
+            cv.rectangle(image, (x1, y1),  
+                      (x1 + h1, y1 + w1),  
+                      (0, 255, 0), 5) 
         for (x,y,w,h) in faces:
             center = (x + w//2, y + h//2)
-            image = cv.ellipse(image, center, (w//2, h//2), 0, 0, 360, faceColor, 4)
+            image = cv.ellipse(image, center, (w//2, h//2), 0, 0, 360, self.faceColor, 4)
             faceROI = frame_gray[y:y+h,x:x+w]
             #-- In each face, detect eyes
             eyes = eyes_cascade.detectMultiScale(faceROI)
             for (x2,y2,w2,h2) in eyes:
                 eye_center = (x + x2 + w2//2, y + y2 + h2//2)
                 radius = int(round((w2 + h2)*0.25))
-                image = cv.circle(image, eye_center, radius, eyeColor, 4)
+                image = cv.circle(image, eye_center, radius, self.eyeColor, 4)
 
 
 
